6d03c92dc27585cc610607bac8d28912806cafac
PabloMazza <pablomazza21@gmail.com>
2020-01-22 21:42:57 -0300

    Start of Hooks - SearchParams

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Pablo", animal: "Human", breed: "ovo" }),
-    React.createElement(Pet),
-    React.createElement(Pet)
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
